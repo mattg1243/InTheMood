9e2ef51522711a169feae643db44125af1e7f3db
Matt Gallucci <mattgallucci97@gmail.com>
2022-11-05 21:07:53 -0700

    added comments, refactoring, PORT is now env variable

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,34 @@
 import express, { Request, Response, NextFunction } from 'express';
 import path from 'path';
-import { getRecommendations } from './utils/spotify';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-// middleware
 import { checkTokenCookie } from './middleware/checkToken';
+import { recommendSongsHandler } from './handlers/Recommend.handlers';
+// load .env variables
 dotenv.config();
-
+const PORT = process.env.PORT;
+// initiate express app
 const app = express();
+// middleware
 app.use(cookieParser());
+app.use(checkTokenCookie);
 
-app.get(
-  '/recommend',
-  checkTokenCookie,
-  async (req: Request<{}, {}, { mood: string }>, res: Response, next: NextFunction) => {
-    try {
-      const recomendations = await getRecommendations('happy', req.cookies.spotifyToken);
-      res.status(200).json(recomendations);
-    } catch (err) {
-      console.error(err);
-    }
-  }
-);
+// server routes
+// this is the route to hit in order to get the recommendations from Spotify
+// see the handler function if you need to see how the response is sent
+app.get('/api/recommend', recommendSongsHandler);
 
-app.get('/features', checkTokenCookie, (req: Request, res: Response, next: NextFunction) => {
+// static routes
+// these routes all send html pages to the browser
+app.get('/features', (req: Request, res: Response, next: NextFunction) => {
   res.sendFile(path.join(__dirname, '../client/features.html'));
 });
 
-// app.get('/token', async (req: Request, res: Response, next: NextFunction) => {
-//   console.log('token req');
-// });
-
-app.get('*', checkTokenCookie, (req: Request, res: Response, next: NextFunction) => {
+app.get('*', (req: Request, res: Response, next: NextFunction) => {
   res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
-app.listen(3000);
+// start the server on port 3000
+app.listen(3000, () => {
+  console.log(`  ---  server listening for requests on port ${PORT}  ---`);
+});
