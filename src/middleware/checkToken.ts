221422a45e626fae5cdc6ccecdee50bfd5a755a3
Matt Gallucci <mattgallucci97@gmail.com>
2022-11-05 19:09:18 -0700

    added token handling middleware

--- src/middleware/checkToken.ts
+++ src/middleware/checkToken.ts
@@ -3,7 +3,8 @@ import { Request, Response, NextFunction } from 'express';
 import { getToken } from '../utils/spotify';
 
 export const checkTokenCookie = async (req: Request, res: Response, next: NextFunction) => {
-  if (req.cookies.spotifyToken) {
+  // check if the client has a valid token
+  if (req.cookies.spotifyToken && new Date(Date.now()) < new Date(req.cookies.spotifyTokenExpiry)) {
     next();
   } else {
     const response = await getToken();
@@ -11,5 +12,7 @@ export const checkTokenCookie = async (req: Request, res: Response, next: NextFu
     const tokenExpiry = new Date(new Date().getTime() + response.expires_in);
     console.log('Expires at: ' + tokenExpiry);
     res.cookie('spotifyToken', token).status(200);
+    res.cookie('spotifyTokenExpiry', tokenExpiry);
+    next();
   }
 };
