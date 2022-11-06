3123ccfe395eabd5a186815b7bc20cd8f438a63a
Matt Gallucci <mattgallucci97@gmail.com>
2022-11-05 21:07:30 -0700

    refactoring, added query string checks for recommend route

---
+++ src/handlers/Recommend.handlers.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from 'express';
+import { getRecommendations } from '../utils/spotify';
+import { Mood } from '../types';
+
+export const recommendSongsHandler = async (
+  req: Request<{}, {}, { mood: Mood }>,
+  res: Response,
+  next: NextFunction
+) => {
+  const mood = req.query.mood;
+  // need to find a better, TypeScript way to do this check
+  if (mood !== 'happy' && mood !== 'sad' && mood !== 'angry') {
+    return res.status(400).json({ message: 'please send mood as either happy, sad, or angry' });
+  }
+  try {
+    const recomendations = await getRecommendations(mood, req.cookies.spotifyToken);
+    res.status(200).json(recomendations);
+  } catch (err) {
+    console.error(err);
+  }
+};
